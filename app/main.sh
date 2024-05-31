1551db3439810151175829b7881b5a7a92b2904a
Nathan Ladwig <netham45@gmail.com>
2024-05-31 18:36:49 -0500

    44100 -> 48000

--- app/main.sh
+++ app/main.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 rm -rf /tmp/* /tmp/.X* /run/dbus/pid /run/avahi-daemon/pid || true
 export PULSE_COOKIE=/var/run/pulse/.config/pulse/cookie
 dbus-daemon --system
 avahi-daemon -D
 pulseaudio --system --disallow-exit -D
-pactl load-module module-rtp-send format=s16le channels=2 rate=44100 source=auto_null.monitor destination=$RTP_TARGET port=$RTP_PORT mtu=1164
+pactl load-module module-rtp-send format=s16le channels=2 rate=48000 source=auto_null.monitor destination=$RTP_TARGET port=$RTP_PORT mtu=1164
 Xtigervnc -desktop "$VNC_DESKTOP_NAME" -geometry "$VNC_GEOMETRY" -listen tcp -ac -SecurityTypes None -AlwaysShared -AcceptKeyEvents -AcceptPointerEvents -SendCutText -AcceptCutText :0 &
 sleep 2
 export DISPLAY=:0.0
